666746ff5d9dcddca60b7de37babbdaf1b422143
TatsuyaOgawa3 <81093535+TatsuyaOgawa3@users.noreply.github.com>
2022-11-15 18:14:47 +0900

    1st
    1st

---
+++ blog/components/footer.js
@@ -0,0 +1,13 @@
+import Logo from 'components/logo'
+import styles from 'styles/footer.module.css'
+
+export default function Footer() {
+  return (
+    <footer className={styles.wrapper}>
+      <div className={styles.flexContainer}>
+        <Logo />
+        [ソーシャル]
+      </div>
+    </footer>
+  )
+}
