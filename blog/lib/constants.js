c0d070f662960d11c9697bdd8ef3a7b7a1c19428
TatsuyaOgawa3 <81093535+TatsuyaOgawa3@users.noreply.github.com>
2022-12-18 18:44:55 +0900

    8,4
    2nd

---
+++ blog/lib/constants.js
@@ -0,0 +1,9 @@
+export const siteMeta = {
+  siteTitle: 'CUBE',
+  siteDesc: 'アウトプットしていくサイト',
+  siteUrl: 'https:/*****',
+  siteLocale: 'ja',
+  siteLang: 'ja_JP',
+  siteType: 'website',
+  siteIcon: '/favicon.png',
+}
